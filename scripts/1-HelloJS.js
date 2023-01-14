357f466d409170290e1daccb3e8b261c4698ab19
vik <sgeerawor@gmail.com>
2023-01-14 10:58:36 +0000

    apply css to js

--- scripts/1-HelloJS.js
+++ scripts/1-HelloJS.js
@@ -1,18 +1,18 @@
 "use strict";
 
 document.write("<h1>document.write() and document.writeln()</h1>")
-document.write("This is a line with document.write().");
-document.write("This is another line with document.write().")
-document.writeln("This is a line with document.writeln().")
-document.writeln("This is line with document.writeln().")
-document.write("This is a line with document.write().")
-document.writeln("This is a line with document.writeln().")
+document.write("This is a line with <strong>document.write()</strong>.");
+document.write("This is another line with <strong>document.write()</strong>.")
+document.writeln("This is a line with <strong>document.writeln()</strong>.")
+document.writeln("This is line with <strong>document.writeln()</strong>.")
+document.write("This is a line with <strong>document.write()</strong>.")
+document.writeln("This is a line with <strong>document.writeln()</strong>.")
 document.write("<h2>Conclusion</h2>")
-document.write("document.writeln() adds a space after the line")
+document.write("<strong>document.writeln()</strong> adds a space after the line")
 
 document.write("<h1>Comments</h1>")
-document.write("<p>//This is a one-line comment</p>")
-document.write("<p>/* <br>" +
+document.write("<p class='comment'>//This is a one-line comment</p>")
+document.write("<p class='comment'>/* <br>" +
     "This is a multi-line comment.<br>" +
     "It can span multiple lines.<br>" +
     "*/</p>")
